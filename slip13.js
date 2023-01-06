1305790c41f45ad4e3db57f5d347be8c63f918ab
Akshada1127 <119119682+Akshada1127@users.noreply.github.com>
2023-01-06 14:57:13 +0530

    Update slip13.js

--- slip13.js
+++ slip13.js
@@ -24,3 +24,28 @@ app.post('/login',function(req,res){
 var server = app.listen(3000,function(){
     console.log("Server is Running");
 });
+
+
+/*index.html
+
+<form action="/upload" enctype="multipart/form-data" method="post">
+    <div class="form-group">
+        <input type="file" class="form-control-file" name="file">
+        <input type="submit" value="Upload File" class="btn btn-default">
+    </div>
+    
+    
+    login.html
+    
+    <html>
+    <head>
+        <title>LOGIN FORM</title>
+    </head>
+    <body>
+        <form action="http://localhost:3000/login" method="post">
+            Username: <br> <input type="text" name="usrname" required> <br><br>
+            Password: <br> <input type="password" name="pass" required> <br><br>
+            <input type="submit" value="LOGIN">
+        </form>
+    </body>
+</html>
